2670d117c5d418a0c4629e0bd7d1c0ff3a501c1b
Diego N. Kunzevich <kunze_diego@hotmail.com>
2016-03-27 17:48:30 -0300

    vibrator

--- www/js/index.js
+++ www/js/index.js
@@ -34,6 +34,10 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function () {
         app.receivedEvent('deviceready');
+        
+        console.log(app.vibrate);
+        app.vibrate(3000);
+        
     },
     // Update DOM on a Received Event
     receivedEvent: function (id) {
